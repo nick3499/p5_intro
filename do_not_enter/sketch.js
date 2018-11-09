e5114b9ab9b38823be1a82c3d0618cf6379eeb09
nick3499 <ntrinen@yahoo.com>
2018-11-08 21:36:57 -0600

    Revised sketch
     * added `let size = 200 + x`
     * revised to `ellipse(x, y, size, size)`
     * revised to `rect(x, y, size*0.75, size*0.15)`

--- do_not_enter/sketch.js
+++ do_not_enter/sketch.js
@@ -9,9 +9,10 @@ setup = () => {
 draw = () => {
   background(1, 186, 240)
   const y = height / 2
+  let size = 200 + x
   fill(237, 34, 93)
-  ellipse(x, y, 200, 200)
+  ellipse(x, y, size, size)
   fill(255)
-  rect(x, y, 150, 30)
+  rect(x, y, size*0.75, size*0.15)
   x = x + 1
 }
