99769a96555a98c7cc76da9c09b35c5d70c4e5eb
nick3499 <ntrinen@yahoo.com>
2018-11-09 16:13:00 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,13 @@
-let toggle = true
-
 setup = () => {
   createCanvas(800, 300)
-  rectMode(CENTER)
+  background(1, 75, 100)
 }
 
 draw = () => {
-  if (mouseIsPressed === true) {
-    toggle = !toggle
-  }
-  if (toggle === true) {
-    background(1, 186, 240)
-  } else {
-    background(250, 150, 50)
-  }
-  let x = width/2
-  let y = height/2
-  let size = 200
-  fill(237, 34, 93)
+  let x = mouseX
+  let y = mouseY
+  let size = 25
+  fill(237, 34, 93, 100)
   noStroke()
   ellipse(x, y, size, size)
-  fill(255)
-  rect(x, y, size*0.75, size*0.15)
 }
