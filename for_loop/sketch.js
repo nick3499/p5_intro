c3ba0be8a027ba1a4bc847e7e92e08872bdb129a
nick3499 <ntrinen@yahoo.com>
2018-11-09 23:44:57 -0600

    Revised ellipse
    Added `random()` to the ellipse x dim. --> `ellipse(dia / 2 + i * dia, dia / 2 + j * dia, dia * random(), dia)`

--- for_loop/sketch.js
+++ for_loop/sketch.js
@@ -9,7 +9,7 @@ draw = () => {
   let dia = 50
   for (let i = 0; i < width / dia; i = i + 1) {
     for (let j = 0; j < height / dia; j = j + 1) {
-      ellipse(dia / 2 + i * dia, dia / 2 + j * dia, dia, dia)
+      ellipse(dia / 2 + i * dia, dia / 2 + j * dia, dia * random(), dia)
     }
   }
 }
